75b65946ee43249f51c4f0026d0e458656747225
Adamshog <64581058+Adamshog@users.noreply.github.com>
2020-05-31 11:23:07 -0700

    email

--- js/script.js
+++ js/script.js
@@ -403,8 +403,8 @@
 
 	
 	//Contact Form Validation
-	if($('#contact-form').length){
-		$('#contact-form').validate({
+	if($('adamshog2000@gmail.com').length){
+		$('adamshog2000@gmail.com').validate({
 			rules: {
 				username: {
 					required: true
